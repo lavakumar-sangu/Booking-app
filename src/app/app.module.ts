8617b5213ceb24c63b91a4bb26e27465168b257c
DennisZazz <lavakumar@zazz.io>
2024-07-14 16:48:26 +0530

    updated node modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatDialogModule } from '@angular/material/dialog';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatNativeDateModule } from '@angular/material/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
@@ -9,14 +14,24 @@ import { BookingComponent } from './booking/booking.component';
 import { BookingDialogComponent } from './booking-dialog/booking-dialog.component';
 
 @NgModule({
-  declarations: [AppComponent, BookingComponent, BookingDialogComponent],
+  declarations: [
+    AppComponent,
+    BookingComponent,
+    BookingDialogComponent
+  ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatDialogModule,
-    FormsModule,
+    MatButtonModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatDatepickerModule,
+    MatNativeDateModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent],
+  entryComponents: [BookingDialogComponent]
 })
 export class AppModule { }
